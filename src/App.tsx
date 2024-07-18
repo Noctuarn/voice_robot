78ad9a1bafe816b45ba171e430b5f430dec4068c
Roman Humenyak <romangumenyak2003@gmail.com>
2024-07-18 11:46:33 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header/Header";
+
+const App = () => {
+  return <main>
+    <Header/>
+  </main>;
+};
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.scss"
+
+const Header = () => {
+  return (
+    <header className={`container ${styles.header}`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A dignissimos maiores dolor quos debitis porro incidunt maxime. Accusantium impedit saepe quidem tempore deserunt numquam! Tenetur cum praesentium id doloribus perferendis?
+    </header>
+  )
+}
+
+export default Header
